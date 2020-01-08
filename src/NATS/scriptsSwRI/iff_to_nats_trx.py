e81a785d20e4b04a7ed7eccc4e064df5e5c616fb
edecarlo-swri <erin.decarlo@swri.org>
2020-01-08 15:58:30 -0600

    Functions to generate gate to runway flight plans
    Created two functions - one to create the gate to runway flight plan directly from iff data and another to create the gate to runway flight plan using internal nats function airportInterface.get_taxi_route_from_A_to_B

--- src/NATS/scriptsSwRI/iff_to_nats_trx.py
+++ src/NATS/scriptsSwRI/iff_to_nats_trx.py
@@ -7,7 +7,7 @@ import numpy as np
 
 track_file = "../../../../Documents/nats_analyses/trx/cs_aal343.trx"
 mfl_file = "../../../../Documents/nats_analyses/trx/cs_aal343_mfl.trx"
-natsSim = NATS_Config(track_file=track_file,max_flt_lev_file = mfl_file)
+natsSim = NATS_Config()
 
 iff_data = utils.read_data_file("../PARA_ATM/sample_data/IFF_SFO_ASDEX_ABC123.csv",record_types=[2,3,4])
 
@@ -16,12 +16,13 @@ for callsign in iff_data[3].callsign.unique():
     departureAirport = iff.get_departure_airport_from_iff(iff_data,callsign)
     arrivalAirport = iff.get_arrival_airport_from_iff(iff_data,callsign)
 
-    flightTakenOff = iff.check_if_flight_has_departed(iff_data,callsign,natsSim,departureAirport)
+    flightTakenOff = iff.check_if_flight_has_departed(iff_data[3],callsign,natsSim,departureAirport)
 
     if not flightTakenOff:
         departureAirportElevation=natsSim.airportInterface.select_airport(departureAirport).getElevation()
         trackData = iff_data[3].loc[((iff_data[3].callsign==callsign) & (iff_data[3].altitude < departureAirportElevation + 50.)),:].copy()
-        txt = iff.create_gate_to_runway_from_iff(trackData,natsSim,departureAirport)
+        txt1 = iff.create_gate_to_runway_from_iff(trackData,natsSim,departureAirport)
+        txt2 = iff.create_gate_to_runway_trx_using_nats(trackData,natsSim,departureAirport)
     else:
         print('Still need to do gate to gate flight plans for landing aircraft.')
     
